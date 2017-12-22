1a1868737791ffc18a6f139424a5daaee2099691
Yuta Saito <kateinoigakukun@gmail.com>
2017-12-22 16:59:45 +0900

    add binary

---
+++ Carthage/Build/iOS/BlurEditor.framework/Headers/BlurEditor.h
@@ -0,0 +1,19 @@
+//
+//  BlurEditor.h
+//  BlurEditor
+//
+//  Created by SaitoYuta on 2017/12/19.
+//  Copyright © 2017年 bangohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlurEditor.
+FOUNDATION_EXPORT double BlurEditorVersionNumber;
+
+//! Project version string for BlurEditor.
+FOUNDATION_EXPORT const unsigned char BlurEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlurEditor/PublicHeader.h>
+
+
